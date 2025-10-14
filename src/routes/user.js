a71e9f75708eb942536543693c292494b357e1fb
munisa <munisa@qburst.com>
2025-10-14 20:24:12 +0530

    add paginated feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from '../models/user.js';
 import ConnectionRequests from '../models/connectionRequest.js';
+import { userAuth } from '../middlewares/auth.js';
 
 const router = express.Router();
 
@@ -68,6 +69,11 @@ router.patch('/user/:userId', async (req, res) => {
 router.get('/user/requests/received', async (req, res) => {
     try {
         let loggedUser = req.user;
+
+        const page = req.page || 1;
+        const limit = req.limit || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
         const connectionRequests = await ConnectionRequests
             .find({ toRequestId: loggedUser._id, status: 'interested' })
             .populate('fromRequestId', USER_DATA_FIELDS);
@@ -105,6 +111,36 @@ router.get('/user/connections', async (req, res) => {
         res.status(200).json({
             message: "Data fetched successfully",
             data: data
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+router.get('/feed', userAuth, async (req, res) => {
+    try {
+        const loggedUser = req.user;
+        const connections = ConnectionRequests.find({
+            $or: [{
+                fromRequestId: loggedUser._id
+            }, {
+                toRequestId: loggedUser._id
+            }]
+        })
+
+        const hideUsers = new Set();
+        connections.forEach((conn) => {
+            hideUsers.add(conn.fromRequestId.toString)
+            hideUsers.add(conn.toRequestId.toString)
+        })
+
+        const feedUsers = await User.find({ _id: { $ne: loggedUser._id, $nin: Array.from(hideUsers) } })
+            .select(USER_DATA_FIELDS).skip(skip).limit(limit);
+
+        res.status(200).json({
+            message: "Data send succes",
+            data: feedUsers
         });
     } catch (err) {
         console.error(err);
